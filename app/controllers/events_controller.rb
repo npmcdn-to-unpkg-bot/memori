a28b4d4c0611506639cd11e7b4cd2027ae538406
Ty Shaikh <anytayyab@gmail.com>
2016-04-27 17:09:36 -0400

    client backend timeline ajaxified

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,28 +2,49 @@ class EventsController < ApplicationController
   before_action :set_memorial
   before_action :require_user
   before_action :require_creator
-  respond_to :html, :js
 
   def new
     @event = Event.new
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def create
-    @event =  @memorial.events.build(params.require(:event).permit(:date, :title, :description, :picture))
+    @event = @memorial.events.build(params.require(:event).permit(:date, :title, :description, :picture))
 
-    if @event.save
-      flash[:notice] = "your event was added."
-      redirect_to edit_memorial_path(@memorial)
-    else
-      flash[:notice] = "problem adding your event."
-      redirect_to edit_memorial_path(@memorial)
+    respond_to do |format|
+      format.html do
+        if @event.save
+          flash[:notice] = "your event was added."
+          redirect_to edit_memorial_path(@memorial)
+        else
+          flash[:notice] = "problem adding your event."
+          redirect_to edit_memorial_path(@memorial)
+        end
+      end
+
+      format.js do
+        @events = @memorial.events
+      end
     end
   end
 
   def destroy
     event = Event.find(params[:id])
     event.destroy if @memorial.events.include?(event)
-    redirect_to :back
+
+    respond_to do |format|
+      format.html do
+        redirect_to :back
+      end
+
+      format.js do
+        @events = @memorial.events
+      end
+    end
+
   end
 
   private
